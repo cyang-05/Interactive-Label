d1e32777c59b50856367679b8ec3be1eca83da5f
cyang-05 <chiashiiyang@gmail.com>
2025-04-01 12:23:34 -0400

    updated code w/edits

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+
+console.log("hi");
+
 $('#nutrition-panel').nutritionLabel({
     showLegacyVersion : false,
+    showCaffeine: false
+    
 })
+
+calorieRow = d3.selectAll(".nf-calories"); // will store .nf-calories now
+
+calorieRow.on('mouseover', function(event, d) {
+        d3.select(this).node().classList.add("highlight");
+        // calorieRow.node().classList.add("highlight");
+    })
+
+
+    .on('mouseout', function(event, d) {
+        d3.select(this).node().classList.remove("highlight");
+        // caloreRow.node().classList.remove("highlight");
+    });
+
+
+
+
+
+nfLines = d3.selectAll(".nf-line");
+
+nfLines.on('mouseover', function(event, d) {
+
+    // console.log(d3.select(this).node().classList)
+    d3.select(this).node().classList.add("lineHighlight");
+    // nfLines.node().classList.add("highlight");
+    })
+
+
+    .on('mouseout', function(event, d) {
+        d3.select(this).node().classList.remove("lineHighlight");
+        // nfLines.node().classList.remove("highlight");
+    });
+
+
+
+
+
+
+
+
+
+
